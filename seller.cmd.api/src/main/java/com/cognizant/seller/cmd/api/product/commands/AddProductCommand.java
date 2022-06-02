d72132de59894f8237e7ae6d11362588fd917724
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2022-06-02 15:38:55 +0200

    Fix AddProductCommand

--- seller.cmd.api/src/main/java/com/cognizant/seller/cmd/api/product/commands/AddProductCommand.java
+++ seller.cmd.api/src/main/java/com/cognizant/seller/cmd/api/product/commands/AddProductCommand.java
@@ -1,19 +1,26 @@
 package com.cognizant.seller.cmd.api.product.commands;
 
 import com.cognizant.user.core.models.Product;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
+@AllArgsConstructor
 @Data
 @Builder
-public class AddProductCommand {
+public class AddProductCommand implements Serializable {
     @TargetAggregateIdentifier
     private String id;
     @NotNull(message = "no product details were supplied")
     @Valid
     private Product product;
+
+    public AddProductCommand(){
+
+    }
 }
