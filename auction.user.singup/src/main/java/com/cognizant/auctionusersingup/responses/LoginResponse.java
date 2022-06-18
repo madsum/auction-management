195013c875d540fb6893ed5bf8d7657c4e2e9183
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2022-06-18 17:42:26 +0200

    Impleamment your singup and authenticatio

---
+++ auction.user.singup/src/main/java/com/cognizant/auctionusersingup/responses/LoginResponse.java
@@ -0,0 +1,26 @@
+package com.cognizant.auctionusersingup.responses;
+
+import com.cognizant.auctionusersingup.entities.User;
+
+public class LoginResponse {
+	
+	private String token;
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+}
---
+++ auction.user.singup/src/test/java/com/cognizant/auctionusersingup/SpringSecurityDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.auctionusersingup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
