c667690c45eac4d8f9efd469b2ed4ef7d3945182
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2022-07-26 11:02:29 +0300

    Cleanup

--- product.query.api/src/main/java/com/cognizant/product/query/api/ProductQueryApplication.java
+++ product.query.api/src/main/java/com/cognizant/product/query/api/ProductQueryApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-//@EnableScheduling
 @SpringBootApplication
 @Import({ AxonConfig.class,  AppMessageQueueConfig.class })
 public class ProductQueryApplication {
