93d9f1f2b4ba09860f9e1f77f902bb7d6a6a4b4e
Masum Islam <101326878+masum-islam84@users.noreply.github.com>
2022-08-18 15:24:59 +0200

    Remove rabbitmq

--- product.query.api/src/main/java/com/cognizant/product/query/api/ProductQueryApplication.java
+++ product.query.api/src/main/java/com/cognizant/product/query/api/ProductQueryApplication.java
@@ -1,13 +1,12 @@
 package com.cognizant.product.query.api;
 
-import com.cognizant.core.configuration.AppMessageQueueConfig;
 import com.cognizant.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ AxonConfig.class,  AppMessageQueueConfig.class })
+@Import({ AxonConfig.class})
 public class ProductQueryApplication {
 
 	public static void main(String[] args) {
